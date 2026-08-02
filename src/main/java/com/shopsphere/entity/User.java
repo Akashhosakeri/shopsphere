29fbca76e27e5332c4556d5fd5e5553b9857af21
Akash Hosakeri <akashhosakeri0615@gmail.com>
2026-08-02 14:42:40 +0530

    Add user entity and role enum

---
+++ src/main/java/com/shopsphere/entity/User.java
@@ -0,0 +1,23 @@
+package com.shopsphere.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private Role role;
+    private Boolean enabled;
+}
